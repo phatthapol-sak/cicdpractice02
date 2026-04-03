1481e7593fbb0f4a7671458ae2d3b2facf408167
Phatthapol <phatthapol.sak@gmail.com>
2026-04-03 14:12:18 +0700

    refractor: Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Images
+        <Image
           className={styles.logo}
           src="/next.svg"
           alt="Next.js logo"
