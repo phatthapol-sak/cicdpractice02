b9c625ebcc4a3bf8054c09baef4b28db7f98d08e
Phatthapol <phatthapol.sak@gmail.com>
2026-04-03 14:23:36 +0700

    feat: Update Code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/nextjstest",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
